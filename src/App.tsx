c04c3a4e1bc989112e4827a61955ef511632ff87
Rishav Mishra <irishavmishra@proton.me>
2025-01-05 17:05:43 +0530

    animation

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,21 @@ function App() {
           color: "#000",
           // transform: "scale(2) translateY(50px)",4
           scale: 2,
-          y: 100,
-          rotate: 1800,
+          // y: 100,
+          // rotate: 1800,
+        }}
+        transition={{
+          delay: 1,
+          // ease: "easeInOut",
+          repeat: Infinity,
+          repeatDelay: 0.5,
+          repeatType: "reverse",
+          type: "spring",
+          bounce: 40,
+          damping: 10,
+          mass: 3,
+          stiffness: 30,
+          velocity: 100,
         }}
         initial={{
           backgroundColor: "#00f",
