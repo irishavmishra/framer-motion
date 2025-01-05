272904e3ba49e89439a15f1f1994d071e16d7324
Rishav Mishra <irishavmishra@proton.me>
2025-01-05 18:37:49 +0530

    adding interactivity hover focus and tap

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,21 @@ import { motion } from "framer-motion";
 
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen text-white">
-      <motion.div
-        className="bg-black w-24 h-24"
-        animate={{
-          // backgroundColor: "#f00",
-          // color: "#000",
-          // transform: "scale(2) translateY(50px)",4
-          // scale: 2,
-          // y: 100,
-          // rotate: 1800,
-
-          scale: [1, 2, 2, 2, 1, 1],
-          rotate: [0, 0, 180, 180, 0],
-          borderRadius: ["0%", "0%", "50%", "50%", "0%", "0%"],
+    <div className="flex items-center flex-col justify-center p-8 min-h-screen text-white">
+      <motion.button
+        className="bg-red-600 h-16 px-8 rounded-full"
+        whileHover={{
+          scale: 2,
         }}
-        transition={{
-          // delay: 1,
-          // // ease: "easeInOut",
-          // repeat: Infinity,
-          // repeatDelay: 0.5,
-          // repeatType: "reverse",
-          // type: "spring",
-          // bounce: 40,
-          // damping: 10,
-          // mass: 3,
-          // stiffness: 30,
-          // velocity: 100,
-
-          ease: "easeInOut",
-          repeat: Infinity,
-          repeatDelay: 1,
-          duration: 5,
+        whileFocus={{
+          scale: 2.2,
+        }}
+        whileTap={{
+          scale: 1.7,
         }}
       >
-        {/* Hello World */}
-      </motion.div>
+        Hover me
+      </motion.button>
     </div>
   );
 }
