3dd63d40a7b5a8230fc1babb2a275c6d5d122b3f
Rishav Mishra <irishavmishra@proton.me>
2025-01-05 17:26:20 +0530

    animation loop with keyframe

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,41 @@ import { motion } from "framer-motion";
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen text-white">
-      <motion.button
-        className="bg-black p-3 rounded-md"
+      <motion.div
+        className="bg-black w-24 h-24"
         animate={{
-          backgroundColor: "#f00",
-          color: "#000",
+          // backgroundColor: "#f00",
+          // color: "#000",
           // transform: "scale(2) translateY(50px)",4
-          scale: 2,
+          // scale: 2,
           // y: 100,
           // rotate: 1800,
+
+          scale: [1, 2, 2, 2, 1, 1],
+          rotate: [0, 0, 180, 180, 0],
+          borderRadius: ["0%", "0%", "50%", "50%", "0%", "0%"],
         }}
         transition={{
-          delay: 1,
-          // ease: "easeInOut",
+          // delay: 1,
+          // // ease: "easeInOut",
+          // repeat: Infinity,
+          // repeatDelay: 0.5,
+          // repeatType: "reverse",
+          // type: "spring",
+          // bounce: 40,
+          // damping: 10,
+          // mass: 3,
+          // stiffness: 30,
+          // velocity: 100,
+
+          ease: "easeInOut",
           repeat: Infinity,
-          repeatDelay: 0.5,
-          repeatType: "reverse",
-          type: "spring",
-          bounce: 40,
-          damping: 10,
-          mass: 3,
-          stiffness: 30,
-          velocity: 100,
-        }}
-        initial={{
-          backgroundColor: "#00f",
-          color: "#fff",
-          scale: 1,
-          y: 0,
-          rotate: 0,
+          repeatDelay: 1,
+          duration: 5,
         }}
       >
-        Hello World
-      </motion.button>
+        {/* Hello World */}
+      </motion.div>
     </div>
   );
 }
